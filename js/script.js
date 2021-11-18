434143fea3aed800661ba83c4220cffb89ee64a2
Abishake-5 <ghanabiswa5@gmail.com>
2021-11-18 09:17:44 -0500

    Different way of creating an array of objects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 let pokemonList = [{name:["bulbasaur","pikachu", "Charmander"]},{height:[.7 , .4, .6]},{types:["Grass" , "Electric", "fire"]}];
+// Would the code in line 1 be considered valid ? It does not give me an error
+//  but this fells more like an array within a objects that's inside of an array
+
+
+let pokemonList = [
+    {name: "bulbasaur", height : 0.7 , types: "Grass"},
+     {name: "Pikachu", height : 0.4 , types: "Electric"},
+      {name: "Charmander", height : 0.6 , types: "Fire"}
+];
