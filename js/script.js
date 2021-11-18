0f695c8387a105b7a9e80ec7ea1412814fb84489
Abishake-5 <ghanabiswa5@gmail.com>
2021-11-18 14:05:06 -0500

    CSS added
    This is not permanent.
    I only added this because the exercise requires so

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-let pokemonList = [{name:["bulbasaur","pikachu", "Charmander"]},{height:[.7 , .4, .6]},{types:["Grass" , "Electric", "fire"]}];
-// Would the code in line 1 be considered valid ? It does not give me an error
-//  but this fells more like an array within a objects that's inside of an array
-
 
 let pokemonList = [
     {name: "bulbasaur", height : 0.7 , types: "Grass"},
      {name: "Pikachu", height : 0.4 , types: "Electric"},
-      {name: "Charmander", height : 0.6 , types: "Fire"}
-];
+      {name: "Charmander", height : 0.6 , types: "Fire"},
+      {name: "Mew", height : 1.04 , types: "synchronize"}
+]
+document.write("<div class='text'>");
+// Putting this div outside of the loop ensurs that it does not creat a new class to go along with the content of text it creats
+for (let i = 0 ; i < pokemonList.length; i++){
+    //  i=0 because in an array the index starts at 0 
+    // i < pokemonList.length is the condition. in this case it's untill i reaches the final index
+    // length; i++ Is the action it adds 1 to i=0 untill the condition is met
+
+    document.write("<p> " + pokemonList[i].name + ": ");
+    document.write( ("( ") + pokemonList[i].height + "m )</p>");
+
+    
+    if (pokemonList[i].height > 1 ){
+        // This checks height of each pokemon as it is still inside the for loop  and the incremetn of i++ still works 
+        document.write("<p>The height of " + (pokemonList[i].name) + " exceeds 1m</p>");
+    }
+}
+document.write("</div>");
