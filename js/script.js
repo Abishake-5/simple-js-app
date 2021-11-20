415f117cff2a52d1c50b0873aefc96eb7d04605c
Abishake-5 <ghanabiswa5@gmail.com>
2021-11-19 19:56:02 -0500

    ForEach instead of for loop

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 
-let pokemonList = [
-    {name: "bulbasaur", height : 0.7 , types: "Grass"},
+ let pokemonRepo = (function (){
+     let pokemonList = [
+    {name: "Bulbasaur", height : 0.7 , types: "Grass"},
      {name: "Pikachu", height : 0.4 , types: "Electric"},
       {name: "Charmander", height : 0.6 , types: "Fire"},
-      {name: "Mew", height : 1.04 , types: "synchronize"}
-]
-document.write("<div class='text'>");
-// Putting this div outside of the loop ensurs that it does not creat a new class to go along with the content of text it creats
-for (let i = 0 ; i < pokemonList.length; i++){
-    //  i=0 because in an array the index starts at 0 
-    // i < pokemonList.length is the condition. in this case it's untill i reaches the final index
-    // length; i++ Is the action it adds 1 to i=0 untill the condition is met
-
-    document.write("<p> " + pokemonList[i].name + ": ");
-    document.write( ("( ") + pokemonList[i].height + "m )</p>");
-
-    
-    if (pokemonList[i].height > 1 ){
-        // This checks height of each pokemon as it is still inside the for loop  and the incremetn of i++ still works 
-        document.write("<p>The height of " + (pokemonList[i].name) + " exceeds 1m</p>");
-    }
+      {name: "Mew", height : 1.04 , types: "Synchronize"}
+];
+/*
+if (pokemonList.height >= 1){
+    document.write("<p>The height of " + (pokemonList.name) + " exceeds 1m</p>");
 }
-document.write("</div>");
+QUESTION 
+How can i make this if statement work ?
+
+
+*/
+
+function getAll(){
+    return pokemonList;
+}
+
+function add(pokemon){
+    pokemonList.push(pokemon)
+}
+
+return {
+    getAll: getAll,
+    add: add
+};
+// This marks the end of pokemonRepo
+ })(); // This is the bit of code  that iniates the functin  ();
+ document.write("<div class='text'>")
+
+ pokemonRepo.getAll().forEach(function(pokemon){
+ document.write("<p> Name:" +pokemon.name + " Height: (" + pokemon.height + ") Types:"+ pokemon.types + "</p>")
+ })
+document.write("</div>")
