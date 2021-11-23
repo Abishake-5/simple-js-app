9ec509bc9d49a64f49239ae8ddb5bd1ada1b169e
Abishake-5 <ghanabiswa5@gmail.com>
2021-11-22 19:58:39 -0500

    Added  Event Listener
    * Added  Event Listener
    * Made sure the button was created form JS
    * Cleaned the code and made comments

--- js/script.js
+++ js/script.js
@@ -1,38 +1,70 @@
 
- let pokemonRepo = (function (){
-     let pokemonList = [
-    {name: "Bulbasaur", height : 0.7 , types: "Grass"},
-     {name: "Pikachu", height : 0.4 , types: "Electric"},
-      {name: "Charmander", height : 0.6 , types: "Fire"},
-      {name: "Mew", height : 1.04 , types: "Synchronize"}
+ let pokemonRepository = (function () {
+     let repository = [
+    {
+    name: "Bulbasaur",
+    height : 0.7 ,
+    types: "Grass",
+    },
+     {
+         name: "Pikachu",
+          height : 0.4 , 
+          types: "Electric"
+        },
+      {
+          name: "Charmander",
+           height : 0.6 , 
+           types: "Fire"
+        },
+      {
+          name: "Mew",
+           height : 1.04 , 
+           types: "Synchronize"
+        },
 ];
-/*
-if (pokemonList.height >= 1){
-    document.write("<p>The height of " + (pokemonList.name) + " exceeds 1m</p>");
-}
-QUESTION 
-How can i make this if statement work ?
 
-
-*/
-
-function getAll(){
-    return pokemonList;
-}
 
 function add(pokemon){
-    pokemonList.push(pokemon)
+    repository.push(pokemon)
+}
+
+function getAll(){
+    return repository;
+}
+
+function addListItem(pokemon){
+let pokemonList = document.querySelector(".pokemon-list");
+let listItem = document.createElement("li");
+// creats an elemnt of li and a button 
+let button = document.createElement("button")
+button.innerHTML = pokemon.name;
+// sets up my button to the name of the pokemon
+button.classList.add("button");
+listItem.appendChild(button);
+// Append meaning add. This is then added to the html or in differnt word being used.
+pokemonList.appendChild(listItem);
+
+button.addEventListener("click",function()
+{
+    return showDetails();
+    // returns the function showDetails which shows an alert as of right now
+});
+function showDetails (pokemon){
+    alert("The button was clicked")
+    // This function will eventually be rewritten to disply bio description of the pokemon
+}
+
 }
 
 return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem:addListItem,
+  
 };
+
 // This marks the end of pokemonRepo
  })(); // This is the bit of code  that iniates the functin  ();
- document.write("<div class='text'>")
-
- pokemonRepo.getAll().forEach(function(pokemon){
- document.write("<p> Name:" +pokemon.name + " Height: (" + pokemon.height + ") Types:"+ pokemon.types + "</p>")
+ pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon)
  })
-document.write("</div>")
