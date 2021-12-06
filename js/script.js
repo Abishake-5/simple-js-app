0df53bd6400c5b178fed5fc29d5c1ef85ffc9d78
Abishake-5 <ghanabiswa5@gmail.com>
2021-12-05 22:45:28 -0500

    POP-UP MODAL
    mainly JS function for displaying modal on click and CSS to style the modal & modal-content

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ let pokemonRepository = (function () {
     return pokemonList;
     
 }
+
   function addListItem(pokemon) {
       // Start of addListItem function
     let pokemonList = document.querySelector(".pokemon-list");
@@ -26,13 +27,15 @@ let pokemonRepository = (function () {
     // Creats a list and a button element
     let button = document.createElement("button");
     button.innerText = pokemon.name;
+    //  console.log(pokemon.name);
     button.classList.add("button-class");
     listpokemon.appendChild(button);
     // Append meaning add. This is then added to HTML
     pokemonList.appendChild(listpokemon);
+
     button.addEventListener("click", function(event) {
-        // This function listens for click and runs showDetials function that contains loadDetails function
       showDetails(pokemon);
+      
     });
   }// End of addListItem Function
 
@@ -48,7 +51,7 @@ let pokemonRepository = (function () {
           detailsUrl: item.url
         };
         add(pokemon);
-        console.log(pokemon);
+        // console.log(pokemon);
       });
       // .catch will catch any errors that did not pass through .then promise function
     }).catch(function (e) {
@@ -71,13 +74,76 @@ let pokemonRepository = (function () {
     });
   }// End of loadDetails Funcion
 
-  function showDetails(item) {
+
+  function showDetails(pokemon) {
+    
       // Start of showDetails Function
-    pokemonRepository.loadDetails(item).then(function () {
-        // This grabs the details from the loadDetails function and log's them in console on click 
-      console.log(item);
+    loadDetails(pokemon).then(function () {
+
+      // This grabs the details from the loadDetails function and log's them in console on click 
+      showModal(pokemon);
+        
+      
     });
   }// End of showDetails Function
+
+
+ function showModal (pokemon){
+// start of show modal
+
+let modalContainer = document.createElement('div');
+modalContainer.classList.add('modal-container')
+document.body.appendChild(modalContainer);
+modalContainer.innerText = '';
+//  Creats the parent container element and appends to the body
+
+let modal = document.createElement('div');
+modal.classList.add('modal');
+modalContainer.appendChild(modal);
+// Modal div appens to container
+
+let closeButtonElement = document.createElement('button');
+closeButtonElement.classList.add('closeButtonElement');
+closeButtonElement.innerText = 'Close';
+modal.appendChild(closeButtonElement);
+closeButtonElement.addEventListener('click', hideModal);
+// This is the close button on the modal
+
+let titleElement = document.createElement('h1');
+titleElement.innerText =pokemon.name;
+
+modal.appendChild(titleElement);
+
+let contentElement = document.createElement('p');
+contentElement.innerText = ('Height: ' + pokemon.height + 'm');
+modal.appendChild(contentElement);
+
+let imgElement = document.createElement('img');
+imgElement.src = pokemon.imageUrl;
+modal.appendChild(imgElement);
+
+
+ function hideModal (){
+   modalContainer.style.display="none";
+   
+ }
+
+ window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      hideModal();  
+    }
+   
+  });
+
+    modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  }// End of showModal
+
 
   return {
     // This is where all the function is returned
@@ -88,14 +154,15 @@ let pokemonRepository = (function () {
     loadList: loadList,
     loadDetails: loadDetails,
    
+   
   };
-})();
 
+
+
+})();
 
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
